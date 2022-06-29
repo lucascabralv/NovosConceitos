c884f3e3ae0429e9244dbaa0a6c01cb99641f590
Lucas Cabral <lucascabralvieira@gmail.com>
2022-06-29 15:10:35 -0300

    Create formatToReal.js

---
+++ Webflow/Format/Currency/formatToReal.js
@@ -0,0 +1,22 @@
+const formatToReal = (el) => {
+  let resp = "";
+  const spltNum = el.text().split(".");
+  let inteiros = "";
+  let j = 0;
+  for(let i = spltNum[0].length - 1; i >= 0; i--){
+    if(j == 3){
+      inteiros = spltNum[0][i] + "." + inteiros;
+      j = 1;
+    } else {
+      inteiros = spltNum[0][i] + inteiros;
+      j++;
+    }
+  }
+  if(spltNum.length == 2){
+    resp = "R$ " + inteiros + "," + spltNum[1];
+  } else {
+    resp = "R$ " + inteiros + "," + "00";
+  }
+  el.text(resp);
+  return resp;
+};

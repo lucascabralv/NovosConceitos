c27df64f8c9b5c2972889b5e4fc4e034965d58a7
Lucas Cabral <lucascabralvieira@gmail.com>
2022-08-04 18:17:50 -0300

    Add createSliderSingle.js

--- Webflow/Slider/createSliderSingle.js
+++ Webflow/Slider/createSliderSingle.js
@@ -1,19 +1,27 @@
-/** createSlider for App Moda
+/** createSliderSingle
  * Creates Slider in Div
  * Moves only one slide per click
  * @param {Element} slides 
  * @param {Element} leftControl 
  * @param {Element} rightControl 
- * @param {Element} controlWrapper
+ * @param {Boolean} hasKeepClass // 0 or 1
+ * @param {Integer} delay // ms
  */
-function createSlider(slides, leftControl, rightControl, controlWrapper){
-  if(slides.length > 1){
-    const slider = slides.parent();
+function createSliderSingle(slides, leftControl, rightControl, hasKeepClass = 0, delay){
+  const slider = slides.parent();
+  let initialItems = Math.floor(slider.width()/slides.outerWidth());
+
+  if(slides.length-hasKeepClass > 1 && slides.length > initialItems){
     const MIN = 0;
-    const MAX = slides.length; 
-    let WIDTH = slider.width();
+    let MAX = slides.length - hasKeepClass - initialItems+1; 
+    let WIDTH = slides.outerWidth();
     let index = 0;
-    
+    // Slider Auto Scroll
+    let slideLoop;
+    if(delay && delay > 0){
+     slideLoop = setInterval(nextSlide, delay);
+    }
+
     function moveSlide(){ slides.css("right", index * WIDTH + "px"); }
     function nextSlide(){
       if(index + 1 < MAX){
@@ -35,20 +43,36 @@ function createSlider(slides, leftControl, rightControl, controlWrapper){
       }
     }
     // CONTROLS CLICK EVENT
-    leftControl.click(function(){ prevSlide(); });
-    rightControl.click(function(){ nextSlide(); });
+    leftControl.click(function(){ prevSlide(); clearInterval(slideLoop); });
+    rightControl.click(function(){ nextSlide(); clearInterval(slideLoop); });
 
     // RESIZE HANDLE
-    function getCardWidth(){ WIDTH = slider.width(); moveSlide() }
-    $(window).resize(getCardWidth);
+    function resizeHandler(){ 
+      initialItems = Math.floor(slider.width()/slides.outerWidth());
+      MAX = slides.length - hasKeepClass - initialItems+1;
+      WIDTH = slides.outerWidth(); 
+      if(slides.length > initialItems){
+        leftControl.css('display', 'block');
+        rightControl.css('display', 'block');
+      } else {
+        leftControl.css('display', 'none');
+        rightControl.css('display', 'none');
+      }
+
+      moveSlide() 
+    }
+    
+    $(window).resize(() => {
+    	resizeHandler();
+    });
 
     // MOBILE FINGER SWIPE DETECTION
-    controlWrapper.on("touchstart", handleTouchStart);    
-    controlWrapper.on('touchmove', handleTouchMove);
+    slider.on("touchstart", handleTouchStart);    
+    slider.on('touchmove', handleTouchMove);
     let xDown = null;                                                        
     let yDown = null;
     function getTouches(evt) {
-      return evt.touches ||         // browser API
+      return evt.touches ||             // browser API
         evt.originalEvent.touches; // jQuery
     }                                                     
     function handleTouchStart(evt) {
@@ -63,15 +87,15 @@ function createSlider(slides, leftControl, rightControl, controlWrapper){
       let xDiff = xDown - xUp;
       let yDiff = yDown - yUp;
       if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
-        if ( xDiff > 0 ) { nextSlide(); }
-        else { prevSlide(); }                       
+        if ( xDiff > 0 ) { nextSlide(); clearInterval(slideLoop); }
+        else { prevSlide(); clearInterval(slideLoop); }                       
       }
       /* reset values */
       xDown = null;
       yDown = null;                                             
     };
   } else {
-    leftControl.remove();
-    rightControl.remove();
+    leftControl.css('display', 'none');
+    rightControl.css('display', 'none');
   }
 };

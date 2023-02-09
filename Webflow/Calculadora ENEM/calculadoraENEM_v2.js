149da82c167730fa1e2a5efda9ed7534b5858c01
Lucas Cabral <lucascabralvieira@gmail.com>
2023-02-09 15:29:23 -0300

    Calc ENEM v2

--- Webflow/Calculadora ENEM/calculadoraENEM_v2.js
+++ Webflow/Calculadora ENEM/calculadoraENEM_v2.js
@@ -20,36 +20,36 @@ function calculadoraENEM() {
   /**--------------------------------------------
    *h/               FIRST STEP
    *---------------------------------------------**/
-  $(".range-slider").on("input", function () {
+  $(".range-slider-enem").on("input", function () {
     nota = $(this).val();
-    $("#Nota").val(nota);
+    $("#Nota-ENEM").val(nota);
     checkNota();
     desconto = calcDesconto(nota);
   });
-  $("#Nota").on("input", function () {
+  $("#Nota-ENEM").on("input", function () {
     nota = $(this).val();
-    $(".range-slider").val(nota);
+    $(".range-slider-enem").val(nota);
     checkNota();
     desconto = calcDesconto(nota);
   });
 
   function checkNota() {
     if (nota < 10) {
-      $("#Nota").css("width", "30px");
+      $("#Nota-ENEM").css("width", "30px");
     } else if (nota < 100) {
-      $("#Nota").css("width", "45px");
+      $("#Nota-ENEM").css("width", "45px");
     } else if (nota < 1000) {
-      $("#Nota").css("width", "60px");
+      $("#Nota-ENEM").css("width", "60px");
     } else if (nota == 1000) {
-      $("#Nota").css("width", "75px");
+      $("#Nota-ENEM").css("width", "75px");
     } else {
       nota = 1000;
-      $("#Nota").val(1000);
-      $(".range-slider").val(1000);
-      $("#Nota").css("width", "120px");
+      $("#Nota-ENEM").val(1000);
+      $(".range-slider-enem").val(1000);
+      $("#Nota-ENEM").css("width", "120px");
     }
   }
-  $("#Interesse-DP .form-calc-dropdown-link[modalidade='EAD']").css(
+  $("#Interesse-DP-ENEM .form-calc-dropdown-link[modalidade='EAD']").css(
     "display",
     "none"
   );
@@ -59,14 +59,14 @@ function calculadoraENEM() {
       modalidade = $(this).text();
       $(".form-calc-modalidade").toggleClass("selected");
     }
-    $("#Interesse-DP .form-calc-dropdown-link").css("display", "block");
+    $("#Interesse-DP-ENEM .form-calc-dropdown-link").css("display", "block");
     if (modalidade == "EAD") {
-      $("#Interesse-DP .form-calc-dropdown-link:not([modalidade='EAD'])").css(
+      $("#Interesse-DP-ENEM .form-calc-dropdown-link:not([modalidade='EAD'])").css(
         "display",
         "none"
       );
     } else {
-      $("#Interesse-DP .form-calc-dropdown-link[modalidade='EAD']").css(
+      $("#Interesse-DP-ENEM .form-calc-dropdown-link[modalidade='EAD']").css(
         "display",
         "none"
       );
@@ -77,27 +77,27 @@ function calculadoraENEM() {
    *h/               SECOND STEP
    *---------------------------------------------**/
 
-  $("#name").on("input", function () {
+  $("#Name-ENEM").on("input", function () {
     nome = $(this).val();
     checkFields();
   });
-  $("#lastname").on("input", function () {
+  $("#Lastname-ENEM").on("input", function () {
     sobrenome = $(this).val();
     checkFields();
   });
-  $("#mobilephone").on("input", function () {
+  $("#Mobilephone-ENEM").on("input", function () {
     telefone = $(this).val();
     checkFields();
   });
-  $("#Email").on("input", function () {
+  $("#Email-ENEM").on("input", function () {
     email = $(this).val();
     checkFields();
   });
-  $("#Escolaridade-DP .form-calc-dropdown-link").click(function () {
-    $("#Escolaridade-DP .form-calc-dropdown-link").removeClass("selected");
+  $("#Escolaridade-DP-ENEM .form-calc-dropdown-link").click(function () {
+    $("#Escolaridade-DP-ENEM .form-calc-dropdown-link").removeClass("selected");
     $(this).addClass("selected");
     escolaridade = $(this).text();
-    $("#Escolaridade-DP .form-calc-dropdown-placeholder").text(escolaridade);
+    $("#Escolaridade-DP-ENEM .form-calc-dropdown-placeholder").text(escolaridade);
     checkFields();
     $(".w-dropdown-toggle").removeClass("w--open");
     $(".w-dropdown-list").removeClass("w--open");
@@ -106,20 +106,20 @@ function calculadoraENEM() {
   /**--------------------------------------------
    *h/               THIRD PAGE
    *---------------------------------------------**/
-  $("#Interesse-DP .form-calc-dropdown-link").click(function () {
-    $("#Interesse-DP .form-calc-dropdown-link").removeClass("selected");
+  $("#Interesse-DP-ENEM .form-calc-dropdown-link").click(function () {
+    $("#Interesse-DP-ENEM .form-calc-dropdown-link").removeClass("selected");
     $(this).addClass("selected");
     interesse = $(this).text();
-    $("#Interesse-DP .form-calc-dropdown-placeholder").text(interesse);
+    $("#Interesse-DP-ENEM .form-calc-dropdown-placeholder").text(interesse);
     checkFields();
     $(".w-dropdown-toggle").removeClass("w--open");
     $(".w-dropdown-list").removeClass("w--open");
   });
-  $("#Pretende-DP .form-calc-dropdown-link").click(function () {
-    $("#Pretende-DP .form-calc-dropdown-link").removeClass("selected");
+  $("#Pretende-DP-ENEM .form-calc-dropdown-link").click(function () {
+    $("#Pretende-DP-ENEM .form-calc-dropdown-link").removeClass("selected");
     $(this).addClass("selected");
     pretende = $(this).text();
-    $("#Pretende-DP .form-calc-dropdown-placeholder").text(pretende);
+    $("#Pretende-DP-ENEM .form-calc-dropdown-placeholder").text(pretende);
     checkFields();
     $(".w-dropdown-toggle").removeClass("w--open");
     $(".w-dropdown-list").removeClass("w--open");
